91fde99383062daad6c11c82fce1bd3c3bbc17fc
xcode8 <529404579@qq.com>
2015-12-29 11:54:22 +0800

    Revert "csy"
    This reverts commit 504b635ea11c3030bb2f7ac4beaa3f6a1c716d46.

--- 金字塔/main.swift
+++ 金字塔/main.swift
@@ -8,44 +8,5 @@
 
 import Foundation
 
-//
-//  main.swift
-//  金字塔
-//
-//  Created by admin on 15/9/28.
-//  Copyright © 2015年 admin. All rights reserved.
-//
-
-import Foundation
-
-var sv: Int=0
-
-for var s=10; s>=0 ;s--
-{
-    //第一个三角
-    for var j=0; j<s; j++
-    {
-        print(" ",terminator:"");
-        
-    }
-    //第二个三角
-    for var i=10; i>=s; i--
-    {
-        print("*",terminator:"");//terminator:" 为空不换行"
-        
-    }
-  
-    sv++
-    //第三个三角
-    for var m=0; m < sv-1; m++
-    {
-        print("*",terminator:"");
-    }
-    
-    print("")
-    
-}
-
-
-
+print("Hello, World!")
 
